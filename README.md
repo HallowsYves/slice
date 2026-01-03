844924f6afa51d9497e1b90e083699cb18fbaab9
HallowsYves <vyves246@gmail.com>
2026-01-02 17:01:28 -0800

    Added Progress bar countdown with Rich

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # slice
 Command line interface pomodoro timer 
+
+
+## Current things that were done:
+* Added nava to support audio
+* Added rich to timer.py to have a bar that works along with the timer.
--- timer.py
+++ timer.py
@@ -1,12 +1,17 @@
 import time
 
+from rich.progress import Progress, BarColumn, TextColumn, TimeRemainingColumn
+
 def run_timer(minutes):
     total_seconds = minutes * 60
-    for x in range(total_seconds, 0, - 1):
-        seconds = x % 60
-        minutes = int(x / 60)
-        print(f"{minutes:02}:{seconds:02}", end='\r')
-        time.sleep(1)
+
+    with Progress(TextColumn("[progress.description]{task.description}"),BarColumn(),TimeRemainingColumn(),) as progress:
+        pomo_bar = progress.add_task(f"Pomo", total=total_seconds)
+        for x in range(total_seconds, 0, - 1):
+            seconds = x % 60
+            minutes = int(x / 60)
+            progress.update(pomo_bar, advance=1)
+            time.sleep(1)
     print("\nBreak Time!")
 
 pomo_length = int(input("Pomodoro Length: "))
